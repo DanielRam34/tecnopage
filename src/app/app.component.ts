062aa29f351a864d25c4e656804fd417dbc18685
Daniel de Jesus Ramos Martinez <ramosmartinezdanieldejesus@gmail.com>
2024-07-07 18:17:35 -0600

    primercomit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Asegúrate de importar CommonModule
 
 @Component({
+  
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule], // Añade CommonModule aqu
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
 })
 export class AppComponent {
-  title = 'tecnoPage';
+  welcome = 'Hola!';
+  tasks = [
+    'Instalar el Angular CLI',
+    'Crear proyecto',
+    'Crear componentes'
+  ]
 }
+
